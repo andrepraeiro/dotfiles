4554664f66b0325890845667c22eeb1d7e16ebec
Praeiro <praeiro@Praeiros-MacBook-Air.local>
2025-06-11 23:14:28 -0400

    nuget

---
+++ lua/plugins/cmp-nuget.lua
@@ -0,0 +1,28 @@
+require("table")
+return {
+    {
+        "nvim-lua/plenary.nvim",
+    },
+    {
+        "nvim-cmp",
+        dependencies = {
+            "PasiBergman/cmp-nuget",
+        },
+
+        opts = function(_, opts)
+            local nuget = require("cmp-nuget")
+            nuget.setup({})
+
+            opts.sources = {}
+            opts.formatting = {}
+            table.insert(opts.sources, 1, { name = "nuget" })
+
+            opts.formatting.format = function(entry, vim_item)
+                if entry.source.name == "nuget" then
+                    vim_item.kind = "NuGet"
+                end
+                return vim_item
+            end
+        end,
+    },
+}
